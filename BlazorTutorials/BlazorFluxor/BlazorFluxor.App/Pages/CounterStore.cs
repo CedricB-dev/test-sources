a887be4faa43b0a755b314747236f54cb62e2ec4
Cedric BLOUIN <cedric.blouin.dev@gmail.com>
2023-11-08 12:45:45 +0100

    Add BlazorFluxor.App

---
+++ BlazorTutorials/BlazorFluxor/BlazorFluxor.App/Pages/CounterStore.cs
@@ -0,0 +1,31 @@
+﻿using Fluxor;
+
+namespace BlazorFluxor.App.Pages;
+
+//[FeatureState]
+public record CounterState
+{
+    public int CurrentCount { get; init; }
+}
+
+public class CounterFeature : Feature<CounterState>
+{
+    public override string GetName() => "Counter";
+
+    protected override CounterState GetInitialState()
+    {
+        return new CounterState
+        {
+            CurrentCount = 0
+        };
+    }
+}
+
+public class IncrementCounterAction { }
+
+public static class CounterReducers
+{
+    [ReducerMethod(typeof(IncrementCounterAction))]
+    public static CounterState ReduceIncrementCounterAction(CounterState state) =>
+        new() { CurrentCount = state.CurrentCount + 1 };
+}
---
+++ BlazorTutorials/BlazorFluxor/BlazorFluxor.App/Pages/FetchDataStore.cs
@@ -0,0 +1,62 @@
+﻿using BlazorFluxor.App.Data;
+using Fluxor;
+
+namespace BlazorFluxor.App.Pages;
+
+[FeatureState]
+public record WeatherState
+{
+    public bool IsLoading { get; init; }
+    public IReadOnlyList<WeatherForecast> Forecasts { get; init; } = new List<WeatherForecast>();
+}
+
+public class FetchDataAction
+{
+}
+
+public class FetchDataResultAction
+{
+    public FetchDataResultAction(IReadOnlyList<WeatherForecast> forecasts)
+    {
+        Forecasts = forecasts;
+    }
+
+    public IReadOnlyList<WeatherForecast> Forecasts { get; init; }
+}
+
+public static class WeatherReducers
+{
+    [ReducerMethod(typeof(FetchDataAction))]
+    public static WeatherState ReduceFetchDataAction(WeatherState _) =>
+        new() { IsLoading = true, Forecasts = Array.Empty<WeatherForecast>() };
+
+    // [ReducerMethod]
+    // public static WeatherState ReduceFetchDataResultAction(
+    //     WeatherState state, FetchDataResultAction action) =>
+    //     new() { IsLoading = false, Forecasts = action.Forecasts };
+}
+
+public class FetchDataResultReducers : Reducer<WeatherState, FetchDataResultAction>
+{
+    public override WeatherState Reduce(WeatherState state, FetchDataResultAction action)
+    {
+        return new() { IsLoading = false, Forecasts = action.Forecasts };
+    }
+}
+
+public class Effects
+{
+    private readonly WeatherForecastService _weatherForecastService;
+
+    public Effects(WeatherForecastService weatherForecastService)
+    {
+        _weatherForecastService = weatherForecastService;
+    }
+
+    [EffectMethod(typeof(FetchDataAction))]
+    public async Task HandleFetchDataAction(IDispatcher dispatcher)
+    {
+        var forecasts = await _weatherForecastService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now));
+        dispatcher.Dispatch(new FetchDataResultAction(forecasts));
+    }
+}
