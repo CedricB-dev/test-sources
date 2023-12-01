0207fe226c798d8af3e3f8b07abb9abba1a871b9
Cedric Blouin <cedric.blouin.dev@gmail.com>
2023-12-01 17:31:17 +0100

    create blazor component in react app

---
+++ BlazorTutorials/BlazorInReact/BlazorInReact.Web/Program.cs
@@ -0,0 +1,24 @@
+using BlazorInReact.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
