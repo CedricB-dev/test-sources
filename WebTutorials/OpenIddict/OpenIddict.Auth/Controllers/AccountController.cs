00144196a7d093f52da15628714fa03d87993ffb
Cedric Blouin <cedric.blouin.dev@gmail.com>
2023-12-30 12:07:00 +0100

    edit code flow

--- WebTutorials/OpenIddict/OpenIddict.Auth/Controllers/AccountController.cs
+++ WebTutorials/OpenIddict/OpenIddict.Auth/Controllers/AccountController.cs
@@ -1,9 +1,27 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Abstractions;
+using OpenIddict.Auth.Infrastructure;
 
 namespace OpenIddict.Auth.Controllers;
 
 public class AccountController : Controller
 {
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+    
+    public AccountController(
+        SignInManager<ApplicationUser> signInManager,
+        UserManager<ApplicationUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+    
     [HttpGet]
     public IActionResult Login(string? returnUrl)
     {
@@ -13,6 +31,46 @@ public class AccountController : Controller
             ReturnUrl = returnUrl
         };
 
+        return View(viewModel);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        var user = await _userManager.FindByNameAsync(viewModel.Username);
+        var result = await _signInManager.PasswordSignInAsync(viewModel.Username, viewModel.Password,
+            viewModel.RememberMe, lockoutOnFailure: true);
+        
+        AuthenticationProperties props = null;
+        if(user is not null && result.Succeeded)
+        {
+            if (viewModel.RememberMe)
+            {
+                props = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.Add(TimeSpan.FromDays(30)),
+                };
+            }
+            
+            var claims = new List<Claim>();
+            var roles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(roles.Select(role => new Claim(OpenIddictConstants.Claims.Role, role)));
+            claims.Add(new(OpenIddictConstants.Claims.Name, user.UserName));
+            claims.Add(new(OpenIddictConstants.Claims.Subject, user.Id.ToString()));
+            var claimsIdentity = new ClaimsIdentity(claims);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme ,claimsPrincipal, props);
+            
+            return LocalRedirect(viewModel.ReturnUrl);
+        }
+        
         return View(viewModel);
     }
 }
--- WebTutorials/OpenIddict/OpenIddict.Auth/Controllers/AuthorizationController.cs
+++ WebTutorials/OpenIddict/OpenIddict.Auth/Controllers/AuthorizationController.cs
@@ -1,5 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using OpenIddict.Abstractions;
 using OpenIddict.Server.AspNetCore;
@@ -34,6 +36,29 @@ public class AuthorizationController : Controller
             return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
         }
 
+        if (request is not null && request.IsAuthorizationCodeFlow())
+        {
+            if (HttpContext.User.Identity is { IsAuthenticated: false })
+            {
+                return Challenge(properties: new AuthenticationProperties
+                {
+                    RedirectUri = ""
+                }, new[] { CookieAuthenticationDefaults.AuthenticationScheme });
+            }
+
+            var userClaims = HttpContext.User.Claims;
+
+            var identity = new ClaimsIdentity(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            identity.AddClaim(OpenIddictConstants.Claims.Subject, request.ClientId ?? throw new InvalidOperationException());
+            identity.AddClaim("test", "abc", OpenIddictConstants.Destinations.AccessToken);
+            identity.SetScopes(request.GetScopes());
+            identity.SetResources(await _scopeManager.ListResourcesAsync(identity.GetScopes()).ToListAsync());
+            identity.AddClaims(userClaims);
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+            
+            return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+        
         throw new InvalidOperationException("The specified grant type is not supported.");
     }
 }
