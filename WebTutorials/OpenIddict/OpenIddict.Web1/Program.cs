48cfa8930fbc723ab615c173315ccbfe6a8bda6f
Cedric Blouin <cedric.blouin.dev@gmail.com>
2023-12-30 22:09:53 +0100

    edit Web1

--- WebTutorials/OpenIddict/OpenIddict.Web1/Program.cs
+++ WebTutorials/OpenIddict/OpenIddict.Web1/Program.cs
@@ -1,10 +1,42 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using OpenIddict.Web1.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+
+builder.Services.AddAuthentication(o =>
+    {
+        o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        o.SignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        o.Authority = "https://localhost:7279";
+        o.ClientId = "web-read";
+        o.ClientSecret = "web-read-secret";
+        o.ResponseType = OpenIdConnectResponseType.Code;
+        //o.Scope.Add( OpenIdConnectScope.OpenId);
+        o.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+        o.Scope.Add( "roles");
+        o.Scope.Add("api.read");
+        //o.Scope.Add(OpenIdConnectScope.OfflineAccess);
+        o.ClaimActions.MapJsonKey("role", "role");
+        //o.GetClaimsFromUserInfoEndpoint = true;
+        o.SaveTokens = true;
+    });
 
 var app = builder.Build();
 
@@ -18,9 +50,15 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
