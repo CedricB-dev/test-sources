c5abe82ff04fa20587e05f3ce0c12f1054acb77f
Cedric Blouin <cedric.blouin.dev@gmail.com>
2024-01-03 17:01:36 +0100

    enable refresh token react app

--- WebTutorials/OpenIddict/openiddict-react-app/src/main.tsx
+++ WebTutorials/OpenIddict/openiddict-react-app/src/main.tsx
@@ -4,18 +4,19 @@ import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import {AuthProvider} from "react-oidc-context";
-import {OidcClientSettings} from "oidc-client-ts";
+import {UserManagerSettings} from "oidc-client-ts";
 import {BrowserRouter} from "react-router-dom";
 
-const oidcConfig : OidcClientSettings = {
+const oidcConfig : UserManagerSettings = {
     authority: 'https://localhost:7279',
     client_id: 'react-app',
     client_secret: 'react-app-secret',
     redirect_uri: 'http://localhost:5173/signin-oidc',
     response_type: 'code',
-    scope: 'openid profile roles api.read',
+    scope: 'openid profile roles api.read offline_access',
     post_logout_redirect_uri: 'http://localhost:5173/signout-oidc',
     loadUserInfo : false,
+    automaticSilentRenew: true,
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
