ef935b27576abbe4d160fd0e5e2471be967c4bef
Cedric Blouin <cedric.blouin.dev@gmail.com>
2024-02-25 14:48:33 +0100

    Nuget Central Management

---
+++ NugetTutorials/DirectoryBuild/DirectoryBuild.Cons1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DirectoryBuild.Lib;
+
+var person = new Person("John Doe", new DateOnly(1980, 1, 1));
+var validator = new PersonValidation();
+var result = validator.Validate(person);
+
+
+Console.WriteLine("Hello, {0}", person);
---
+++ NugetTutorials/DirectoryBuild/DirectoryBuild.Lib/Person.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace DirectoryBuild.Lib;
+
+public class Person
+{
+    public Person(string name, DateOnly? birthDay)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        BirthDay = birthDay;
+    }
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateOnly? BirthDay { get; set; }
+    
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, BirthDay: {BirthDay}";
+    }
+}
+
+
+public class PersonValidation : AbstractValidator<Person>
+{
+    public PersonValidation()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    }
+}
