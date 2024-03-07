c23af3a5e23f91bbd8dd673fa48cb68ccf44bf13
Cedric Blouin <cedric.blouin.dev@gmail.com>
2024-03-07 23:01:57 +0100

    edit

---
+++ WebTutorials/vue-app/vue-app/src/components/CountComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+
+const props = defineProps<{ count: number }>()
+
+onMounted(() => {
+  console.log("CountComponent mounted " + props.count)
+});
+
+</script>
+
+<template>
+  <p>{{count}}</p>
+</template>
+
+<style scoped>
+
+</style>
--- WebTutorials/vue-app/vue-app/src/components/HelloWorld.vue
+++ WebTutorials/vue-app/vue-app/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import CountComponent from "./CountComponent.vue";
 
 defineProps<{ msg: string }>()
 
@@ -11,6 +12,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <CountComponent :count="count" />
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
